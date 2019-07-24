426fa914d04d876751ab540875a0f520ca06744f
Sunn Yao <sunyour@gmail.com>
2019-07-25 02:14:41 +0800

    精简tabnine配置

--- config.el
+++ config.el
@@ -154,21 +154,9 @@
       (funcall quote-fn note)))))
 
 (def-package! company-tabnine
+  :when (featurep! :completion company)
   :config
-  (add-to-list 'company-backends #'company-tabnine)
-  ;; Trigger completion immediately.
-  (setq company-idle-delay 0)
-
-  ;; Number the candidates (use M-1, M-2 etc to select completions).
-  (setq company-show-numbers t)
-
-  ;; Use the tab-and-go frontend.
-  ;; Allows TAB to select and complete at the same time.
-  (company-tng-configure-default)
-  (setq company-frontends
-        '(company-tng-frontend
-          company-pseudo-tooltip-frontend
-          company-echo-metadata-frontend)))
+  (add-to-list 'company-backends #'company-tabnine))
 
 ;; define environmental variable for some works
 (setenv "PKG_CONFIG_PATH"
@@ -182,6 +170,3 @@
 (pushnew! initial-frame-alist
           '(width . 200)
           '(height . 55))
-
-;; 启动python虚拟环境
-;; (pyvenv-workon "myvw-3.7")
