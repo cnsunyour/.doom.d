61ce3fe0a7ba7b85ac976f5058bdc5a5fdc6de13
Sunn Yao <sunyour@gmail.com>
2019-08-09 23:44:12 +0800

    修改绑定快捷键为doom-emacs风格

--- +bindings.el
+++ +bindings.el
@@ -1,25 +1,28 @@
 ;;;  -*- lexical-binding: t; -*-
 
 ;; 对换Command和Option键
-(when (eq system-type 'darwin)
-  (setq mac-command-modifier 'meta)
-  (setq mac-option-modifier 'super))
+;; (when (eq system-type 'darwin)
+;;   (setq mac-command-modifier 'meta)
+;;   (setq mac-option-modifier 'super))
 
 ;; general keybindings
-(map!
- (:leader
-   (:desc "Org Agenda" :gnv "a" #'org-agenda)
-   (:desc "Open CFW Calendar" :gnv "oc" #'cfw:open-org-calendar))
+(map! (:leader
+        (:desc "Org Agenda" :gnv "a" #'org-agenda)
+        (:desc "CFW Calendar" :gnv "oc" #'cfw:open-org-calendar))
 
- (:after org
-   (:map org-mode-map "C-c o" #'org-pomodoro))
- (:after org-agenda
-   (:map org-agenda-mode-map "C-c o" #'org-pomodoro))
- )
+      (:after org
+        (:map org-mode-map "C-c o" #'org-pomodoro))
+      (:after org-agenda
+        (:map org-agenda-mode-map "C-c o" #'org-pomodoro))
+
+      (:i "C-h" (kbd "<left>"))
+      (:i "C-l" (kbd "<right>"))
+      (:i "C-j" (kbd "<down>"))
+      (:i "C-k" (kbd "<up>")))
 
 ;; set initial state to emacs for org-agenda
 ;; (add-hook! org-agenda-mode
-  ;; (evil-set-initial-state 'org-agenda-mode 'emacs))
+;;   (evil-set-initial-state 'org-agenda-mode 'emacs))
 
 
 ;; smartparens key-binding
@@ -51,50 +54,49 @@ respectively."
             (double-quote . "\"")
             (back-quote . "`")))
 
-(bind-keys
- :map smartparens-mode-map
- ("C-M-a" . sp-beginning-of-sexp)
- ("C-M-e" . sp-end-of-sexp)
+(bind-keys :map smartparens-mode-map
+           ("C-M-a" . sp-beginning-of-sexp)
+           ("C-M-e" . sp-end-of-sexp)
 
- ("C-<down>" . sp-down-sexp)
- ("C-<up>"   . sp-up-sexp)
- ("M-<down>" . sp-backward-down-sexp)
- ("M-<up>"   . sp-backward-up-sexp)
+           ("C-<down>" . sp-down-sexp)
+           ("C-<up>"   . sp-up-sexp)
+           ("M-<down>" . sp-backward-down-sexp)
+           ("M-<up>"   . sp-backward-up-sexp)
 
- ("C-M-f" . sp-forward-sexp)
- ("C-M-b" . sp-backward-sexp)
+           ("C-M-f" . sp-forward-sexp)
+           ("C-M-b" . sp-backward-sexp)
 
- ("C-M-n" . sp-next-sexp)
- ("C-M-p" . sp-previous-sexp)
+           ("C-M-n" . sp-next-sexp)
+           ("C-M-p" . sp-previous-sexp)
 
- ("C-S-f" . sp-forward-symbol)
- ("C-S-b" . sp-backward-symbol)
+           ("C-S-f" . sp-forward-symbol)
+           ("C-S-b" . sp-backward-symbol)
 
- ("C-<right>" . sp-forward-slurp-sexp)
- ("M-<right>" . sp-forward-barf-sexp)
- ("C-<left>"  . sp-backward-slurp-sexp)
- ("M-<left>"  . sp-backward-barf-sexp)
+           ("C-<right>" . sp-forward-slurp-sexp)
+           ("M-<right>" . sp-forward-barf-sexp)
+           ("C-<left>"  . sp-backward-slurp-sexp)
+           ("M-<left>"  . sp-backward-barf-sexp)
 
- ("C-M-t" . sp-transpose-sexp)
- ("C-M-k" . sp-kill-sexp)
- ("C-S-k"   . sp-kill-hybrid-sexp)
- ("M-S-k"   . sp-backward-kill-sexp)
- ("C-M-w" . sp-copy-sexp)
- ("C-M-d" . delete-sexp)
+           ("C-M-t" . sp-transpose-sexp)
+           ("C-M-k" . sp-kill-sexp)
+           ("C-S-k"   . sp-kill-hybrid-sexp)
+           ("M-S-k"   . sp-backward-kill-sexp)
+           ("C-M-w" . sp-copy-sexp)
+           ("C-M-d" . delete-sexp)
 
- ("M-<backspace>" . backward-kill-word)
- ("C-<backspace>" . sp-backward-kill-word)
- ([remap sp-backward-kill-word] . backward-kill-word)
+           ("M-<backspace>" . backward-kill-word)
+           ("C-<backspace>" . sp-backward-kill-word)
+           ([remap sp-backward-kill-word] . backward-kill-word)
 
- ("M-[" . sp-backward-unwrap-sexp)
- ("M-]" . sp-unwrap-sexp)
+           ("M-[" . sp-backward-unwrap-sexp)
+           ("M-]" . sp-unwrap-sexp)
 
- ("C-x C-t" . sp-transpose-hybrid-sexp)
+           ("C-x C-t" . sp-transpose-hybrid-sexp)
 
- ("C-x ("  . wrap-with-parens)
- ("C-x ["  . wrap-with-brackets)
- ("C-x {"  . wrap-with-braces)
- ("C-x '"  . wrap-with-single-quotes)
- ("C-x \"" . wrap-with-double-quotes)
- ("C-x _"  . wrap-with-underscores)
- ("C-x `"  . wrap-with-back-quotes))
+           ("C-x ("  . wrap-with-parens)
+           ("C-x ["  . wrap-with-brackets)
+           ("C-x {"  . wrap-with-braces)
+           ("C-x '"  . wrap-with-single-quotes)
+           ("C-x \"" . wrap-with-double-quotes)
+           ("C-x _"  . wrap-with-underscores)
+           ("C-x `"  . wrap-with-back-quotes))
