67ddbbe8dd05ab3790d24e76eefd8fded2736cde
Sunn Yao <sunyour@gmail.com>
2019-08-26 11:43:28 +0800

    懒猫插件全部用use-package方式调用

--- packages.el
+++ packages.el
@@ -20,6 +20,29 @@
 (package! alert)
 (package! symbol-overlay)
 (package! exec-path-from-shell)
-(package! youdao-dictionary)
+(package! youdao-dictionary
+  :recipe (:host github :repo "cnsunyour/youdao-dictionary.el"))
 (package! google-translate)
-(package! company-go :disable t)
+(package! theme-changer)
+(package! pinentry)
+(package! fuz)
+
+;; manateelazycat's packages
+(package! company-english-helper :disable t
+  :recipe (:host github :repo "manateelazycat/company-english-helper"))
+(package! insert-translated-name
+  :recipe (:host github :repo "manateelazycat/insert-translated-name"))
+(package! sdcv
+  :recipe (:host github :repo "manateelazycat/sdcv"))
+(package! awesome-tab :disable t
+  :recipe (:host github :repo "manateelazycat/awesome-tab"))
+(package! aweshell :disable t
+  :recipe (:host github :repo "manateelazycat/aweshell"))
+(package! awesome-pair :disable t
+  :recipe (:host github :repo "manateelazycat/awesome-pair"))
+(package! highlight-matching-tag :disable t
+  :recipe (:host github :repo "manateelazycat/highlight-matching-tag"))
+(package! instant-rename-tag :disable t
+  :recipe (:host github :repo "manateelazycat/instant-rename-tag"))
+;; (package! snails
+;;   :recipe (:host github :repo "manateelazycat/snails"))
