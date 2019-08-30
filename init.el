fb87b7215cb10c60f6217f183faa771ae439872d
Sunn Yao <sunyour@gmail.com>
2019-08-30 11:51:00 +0800

    恢复使用pass

--- init.el
+++ init.el
@@ -84,7 +84,7 @@
        macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
-       pass              ; password manager for nerds
+       ;; pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
