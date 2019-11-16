4a4f2489dafc95e98bd08137d2e35d5f2164f340
Sunn Yao <sunyour@gmail.com>
2019-11-16 20:21:42 +0800

    telega中的add-hook改为:hook中，看起来更优雅

--- config.el
+++ config.el
@@ -179,22 +179,23 @@
   :bind ("C-c t" . #'telega)
   :init
   (unless (display-graphic-p) (setq telega-use-images nil))
+  :hook
+  ('telega-root-mode . #'evil-emacs-state)
+  ('telega-chat-mode . #'evil-emacs-state)
+  ('telega-chat-mode . #'yas-minor-mode)
+  ('telega-chat-mode . (lambda ()
+                              (set-company-backend! 'telega-chat-mode
+                                (append '(telega-company-emoji
+                                          telega-company-username
+                                          telega-company-hashtag)
+                                        (when (telega-chat-bot-p telega-chatbuf--chat)
+                                          '(telega-company-botcmd))))
+                              (company-mode 1)))
+  ('telega-chat-pre-message . #'telega-msg-ignore-blocked-sender)
   :config
   (setq telega-proxies
         (list '(:server "127.0.0.1" :port 1086 :enable t
                         :type (:@type "proxyTypeSocks5"))))
-  (add-hook! '(telega-root-mode-hook telega-chat-mode-hook) #'evil-emacs-state)
-  (add-hook! 'telega-chat-mode-hook :append
-             #'yas-minor-mode
-             (lambda ()
-               (set-company-backend! 'telega-chat-mode
-                 (append '(telega-company-emoji
-                           telega-company-username
-                           telega-company-hashtag)
-                         (when (telega-chat-bot-p telega-chatbuf--chat)
-                           '(telega-company-botcmd))))
-               (company-mode 1)))
-  (add-hook 'telega-chat-pre-message-hook #'telega-msg-ignore-blocked-sender)
   (set-popup-rule! "^\\*Telega Root"
     :side 'right :size 100 :quit nil :modeline t)
   (set-popup-rule! "^◀\\(\\[\\|<\\|{\\).*\\(\\]\\|>\\|}\\)"
