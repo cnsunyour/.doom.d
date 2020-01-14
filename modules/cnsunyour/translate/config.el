037f209a79dae93b07b8f23d96baba0dd2685c3c
Sunn Yao <sunyour@gmail.com>
2020-01-14 20:16:27 +0800

    原 api 接口仍然可用，暂时不使用新接口

--- modules/cnsunyour/translate/config.el
+++ modules/cnsunyour/translate/config.el
@@ -61,9 +61,9 @@
   :config
   (set-popup-rule! "^\\*Youdao Dictionary\\*" :side 'right :size 0.4 :select t)
   ;; 设定youdao api id和key
-  (let ((credentials (auth-source-user-and-password "youdao-api")))
-    (setq youdao-dictionary-app-key (car credentials)
-          youdao-dictionary-secret-key (cadr credentials)))
+  ;; (let ((credentials (auth-source-user-and-password "youdao-api")))
+  ;;   (setq youdao-dictionary-app-key (car credentials)
+  ;;         youdao-dictionary-secret-key (cadr credentials)))
   ;; Enable Cache
   (setq url-automatic-caching t)
   ;; Enable Chinese word segmentation support (支持中文分词)
