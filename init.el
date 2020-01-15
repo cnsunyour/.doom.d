b3d341abe149284f701b36c49985a8c3cba26a81
Sunn Yao <sunyour@gmail.com>
2020-01-16 00:50:05 +0800

    add ivy +prescient flag

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
        (company +childframe)           ; the ultimate code completion backend
        ;;(helm +childframe)              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +fuzzy +childframe +icons)               ; a search engine for love and life
+       (ivy +fuzzy +prescient +childframe +icons)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
