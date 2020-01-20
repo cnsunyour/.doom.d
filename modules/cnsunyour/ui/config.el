5aa6d58fbee55105afc90c7acc661035c564285f
Sunn Yao <sunyour@gmail.com>
2020-01-20 19:30:36 +0800

    change default emoji font on linux

--- modules/cnsunyour/ui/config.el
+++ modules/cnsunyour/ui/config.el
@@ -20,7 +20,10 @@
                        :size (if (cnsunyour/is-large-display-p)
                                  (* large-font-size .8)
                                (* small-font-size .8)))
-          (font-spec :family "Symbola"))))
+          (font-spec :family (if (member "Noto Color Emoji"
+                                         (font-family-list))
+                                 "Noto Color Emoji"
+                               "Symbola")))))
 
 ;; 设定popup的窗口形式为右侧开启，宽度为40%
 ;; (set-popup-rule! "^\\*" :side 'right :size 0.5 :select t)
