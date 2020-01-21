92d52f0d09ef73d2d604c7ffa04a202ec2779f3b
Sunn Yao <sunyour@gmail.com>
2020-01-22 00:08:37 +0800

    enable pass +auth flag

--- init.el
+++ init.el
@@ -92,7 +92,7 @@
        macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
-       pass              ; password manager for nerds
+       (pass +auth)              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
