a5a294a9f3954a49675ebe0b42b5b588e948291a
Sunn Yao <sunyour@gmail.com>
2020-01-24 08:52:50 +0800

    Modify comment-edit's popup rule
    and,
    use map! instead define-key

--- modules/cnsunyour/editor/config.el
+++ modules/cnsunyour/editor/config.el
@@ -2,8 +2,10 @@
 
 ;; Edit comment or docstring or code block inside them with your favorite mode.
 (use-package! comment-edit
+  :defer t
   :custom
   (comment-edit-default-mode 'markdown-mode)
+  :init
+  (map! :map prog-mode-map "C-c '" #'comment-edit)
   :config
-  (set-popup-rule! "^\\*edit-indirect " :side 'right :size 0.5 :select t :quit nil)
-  (define-key prog-mode-map (kbd "C-c '") #'comment-edit))
+  (set-popup-rule! "^\\*edit-indirect " :side 'right :size .4 :select t :quit nil))
