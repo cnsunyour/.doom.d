ad912ae728e4e89f36f60027867a160c166ae99e
Sunn Yao <sunyour@gmail.com>
2020-02-02 01:08:56 +0800

    Mod: set diff line spacing on org-agenda view

--- modules/cnsunyour/org/config.el
+++ modules/cnsunyour/org/config.el
@@ -296,3 +296,30 @@ See `org-capture-templates' for more information."
                         (org-agenda-todo-ignore-timestamp t)
                         (org-agenda-todo-ignore-with-date t))))))))
 
+;; set different line spacing on org-agenda view
+(defun my:org-agenda-time-grid-spacing ()
+  "Set different line spacing w.r.t. time duration."
+  (save-excursion
+    (let* ((background (alist-get 'background-mode (frame-parameters)))
+           (background-dark-p (string= background "dark"))
+           (colors (if background-dark-p
+                       (list "#aa557f" "DarkGreen" "DarkSlateGray" "DarkSlateBlue")
+                     (list "#F6B1C3" "#FFFF9D" "#BEEB9F" "#ADD5F7")))
+           pos
+           duration)
+      (nconc colors colors)
+      (goto-char (point-min))
+      (while (setq pos (next-single-property-change (point) 'duration))
+        (goto-char pos)
+        (when (and (not (equal pos (point-at-eol)))
+                   (setq duration (org-get-at-bol 'duration)))
+          (let ((line-height (if (< duration 30) 1.0 (+ 0.5 (/ duration 60))))
+                (ov (make-overlay (point-at-bol) (1+ (point-at-eol)))))
+            (overlay-put ov 'face `(:background ,(car colors)
+                                                :foreground
+                                                ,(if background-dark-p "black" "white")))
+            (setq colors (cdr colors))
+            (overlay-put ov 'line-height line-height)
+            (overlay-put ov 'line-spacing (1- line-height))))))))
+
+(add-hook 'org-agenda-finalize-hook #'my:org-agenda-time-grid-spacing)
