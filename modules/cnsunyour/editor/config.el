7f042b839d5f68b1e41ee25e81a2dac089214746
Sunn Yao <sunyour@gmail.com>
2020-02-25 23:56:59 +0800

    Mod: comment-edit rename to separedit

--- modules/cnsunyour/editor/config.el
+++ modules/cnsunyour/editor/config.el
@@ -1,11 +1,11 @@
 ;;; cnsunyour/editor/config.el -*- lexical-binding: t; -*-
 
 ;; Edit comment or docstring or code block inside them with your favorite mode.
-(use-package! comment-edit
+(use-package! separedit
   :defer t
   :custom
-  (comment-edit-default-mode 'markdown-mode)
+  (separedit-default-mode 'markdown-mode)
   :init
-  (map! :map prog-mode-map "C-c '" #'comment-edit)
+  (map! :map prog-mode-map "C-c '" #'separedit)
   :config
-  (set-popup-rule! "^\\*edit-indirect " :side 'right :size .4 :select t :quit nil))
+  (set-popup-rule! "^\\*edit-indirect " :side 'right :size .5 :select t :quit nil))
