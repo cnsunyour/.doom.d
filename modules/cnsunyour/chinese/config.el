e4aa79b72fd743c1ec103f41d60b27d4934ca1f4
Sunn Yao <sunyour@gmail.com>
2020-03-16 14:30:40 +0800

    定义一个 advice 以解决 posframe 偶尔吃字的问题。

--- modules/cnsunyour/chinese/config.el
+++ modules/cnsunyour/chinese/config.el
@@ -87,6 +87,16 @@ unwanted space when exporting org-mode to hugo markdown."
   (default-input-method "rime")
   (rime-show-candidate 'posframe)
   :config
+  (defadvice! +rime--posframe-display-result-a (args)
+    "给 `rime--posframe-display-result' 传入的字符串加一个全角空
+格，以解决 `posframe' 偶尔吃字的问题。"
+    :filter-args #'rime--posframe-display-result
+    (cl-destructuring-bind (result) args
+      (let ((newresult (if (string-blank-p result)
+                           result
+                         (concat result "　"))))
+        (list newresult))))
+
   (load! "+rime-probe-english"))
 
 
