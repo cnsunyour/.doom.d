16e8801417973b7a43f035df14efa34a661c2ec0
Sunn Yao <sunyour@gmail.com>
2020-03-18 21:13:08 +0800

    `package!` name should be same with repo's name.

--- modules/cnsunyour/chinese/packages.el
+++ modules/cnsunyour/chinese/packages.el
@@ -6,7 +6,7 @@
 (package! pangu-spacing :disable t)
 (package! liberime
   :recipe (:host github :repo "DogLooksGood/liberime" :files ("CMakeLists.txt" "Makefile" "src" "*.el")))
-(package! rime
+(package! emacs-rime
   :pin "5944c530eadb806b0f940142d9cb05a9be6c8941"
   :recipe (:host github :repo "DogLooksGood/emacs-rime" :files ("rime.el")))
 (package! fcitx)
