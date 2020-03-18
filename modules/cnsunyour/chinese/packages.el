f0613591f513afc593867d0d478f8f62513f940c
Sunn Yao <sunyour@gmail.com>
2020-03-18 21:15:41 +0800

    Fix: `package!` name same with repo name

--- modules/cnsunyour/chinese/packages.el
+++ modules/cnsunyour/chinese/packages.el
@@ -6,7 +6,7 @@
 (package! pangu-spacing :disable t)
 ;; (package! liberime
 ;;   :recipe (:host github :repo "DogLooksGood/liberime" :files ("CMakeLists.txt" "Makefile" "src" "*.el")))
-(package! rime
+(package! emacs-rime
   :recipe (:host github :repo "DogLooksGood/emacs-rime" :files ("Makefile" "lib.c" "rime.el")))
 (package! fcitx)
 (package! ace-pinyin)
