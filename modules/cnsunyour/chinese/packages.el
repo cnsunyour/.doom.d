bee3491c76e48648432cf4b691e2212d783d36b1
Sunn Yao <sunyour@gmail.com>
2020-03-18 20:33:06 +0800

    Use old version: dependency on liberime.

--- modules/cnsunyour/chinese/packages.el
+++ modules/cnsunyour/chinese/packages.el
@@ -7,6 +7,7 @@
 (package! liberime
   :recipe (:host github :repo "DogLooksGood/liberime" :files ("CMakeLists.txt" "Makefile" "src" "*.el")))
 (package! rime
+  :pin "5944c530eadb806b0f940142d9cb05a9be6c8941"
   :recipe (:host github :repo "DogLooksGood/emacs-rime" :files ("rime.el")))
 (package! fcitx)
 (package! ace-pinyin)
