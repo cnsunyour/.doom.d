f576bcccff6771374abd6e29ea3567fc9f2c28b3
Sunn Yao <sunyour@gmail.com>
2020-03-22 04:15:53 +0800

    Modify some functions's docstring.

--- modules/cnsunyour/chinese/+rime-probe-english.el
+++ modules/cnsunyour/chinese/+rime-probe-english.el
@@ -3,14 +3,14 @@
 (defun +rime-predicate-after-ascii-char-p ()
   "If the cursor is after a ascii character.
 
-Can be used in `rime-disable-predicates' or `rime-inline-predicates'."
+Can be used in `rime-disable-predicates' and `rime-inline-predicates'."
   (and (> (point) (save-excursion (back-to-indentation) (point)))
        (looking-back "[a-zA-Z0-9\x21-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]" 1)))
 
 (defun +rime-predicate-current-input-punctuation-p ()
   "If the current charactor entered is a punctuation.
 
-Can be used in `rime-disable-predicates' or `rime-inline-predicates'."
+Can be used in `rime-disable-predicates' and `rime-inline-predicates'."
   (and rime--current-input-key
        (or (and (<= #x21 rime--current-input-key) (<= rime--current-input-key #x2f))
            (and (<= #x3a rime--current-input-key) (<= rime--current-input-key #x40))
@@ -20,7 +20,7 @@ Can be used in `rime-disable-predicates' or `rime-inline-predicates'."
 (defun +rime-predicate-puncutuation-after-space-cc-p ()
   "If input a punctuation after a Chinese charactor with whitespace.
 
-Can be used in `rime-disable-predicates' or `rime-inline-predicates'.\""
+Can be used in `rime-disable-predicates' and `rime-inline-predicates'.\""
   (and (> (point) (save-excursion (back-to-indentation) (point)))
        (+rime-predicate-current-input-punctuation-p)
        (looking-back "\\cc +" 2)))
@@ -28,7 +28,7 @@ Can be used in `rime-disable-predicates' or `rime-inline-predicates'.\""
 (defun +rime-predicate-puncutuation-after-ascii-p ()
   "If input a punctuation after a ascii charactor with whitespace.
 
-Can be used in `rime-disable-predicates' or `rime-inline-predicates'."
+Can be used in `rime-disable-predicates' and `rime-inline-predicates'."
   (and (+rime-predicate-current-input-punctuation-p)
        (+rime-predicate-after-ascii-char-p)))
 
@@ -37,7 +37,7 @@ Can be used in `rime-disable-predicates' or `rime-inline-predicates'."
 
 \"Button\" means that positon is not editable.
 
-Can be used in `rime-disable-predicates' or `rime-inline-predicates'."
+Can be used in `rime-disable-predicates' and `rime-inline-predicates'."
   (button-at (point)))
 
 (defun +rime-predicate-beancount-p ()
@@ -47,7 +47,7 @@ Determines whether current buffer's `major-mode' is
 `beancount-mode', and the cursor is at the beginning of the
 line.
 
-Can be used in `rime-disable-predicates' or `rime-inline-predicates'."
+Can be used in `rime-disable-predicates' and `rime-inline-predicates'."
   (when (derived-mode-p 'beancount-mode)
     (not (or (nth 3 (syntax-ppss))
              (nth 4 (syntax-ppss))))))
