2f36ef1b9503e008f2858f873d1c9ec09f897b14
Sunn Yao <sunyour@gmail.com>
2020-03-21 14:40:59 +0800

    Enable `:completion (ivy +childframe)` flag

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
        (company +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +icons)               ; a search engine for love and life
+       (ivy +childframe +icons)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
