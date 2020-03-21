707aad0e9c7f592a858371469c2fe6af685c4fd4
Sunn Yao <sunyour@gmail.com>
2020-03-21 09:28:45 +0800

    Use buffer-local var `rime-disable-predicates`

--- modules/cnsunyour/chinese/+rime-probe-english.el
+++ modules/cnsunyour/chinese/+rime-probe-english.el
@@ -16,16 +16,18 @@ line."
     (not (or (nth 3 (syntax-ppss))
              (nth 4 (syntax-ppss))))))
 
-(defun +rime-predicate-multi-mode-english-p()
-  "Using different predicates in different modes."
-  (if (derived-mode-p 'telega-chat-mode
-                      'text-mode)
-      (rime-predicate-auto-english-p)
-    (or (rime-predicate-after-alphabet-char-p)
-        (rime-predicate-prog-in-code-p)
-        (+rime-predicate-beancount-p))))
 
-(setq rime-disable-predicates '(rime-predicate-evil-mode-p
-                                +rime-predicate-button-at-point-p
-                                rime-predicate-punctuation-line-begin-p
-                                +rime-predicate-multi-mode-english-p))
+(setq-default rime-disable-predicates
+              '(+rime-predicate-button-at-point-p
+                rime-predicate-evil-mode-p
+                rime-predicate-punctuation-line-begin-p
+                rime-predicate-after-alphabet-char-p
+                rime-predicate-prog-in-code-p
+                +rime-predicate-beancount-p))
+
+(add-hook! '(telega-chat-mode text-mode)
+  (lambda () (setq-local rime-disable-predicates
+                    '(+rime-predicate-button-at-point-p
+                      rime-predicate-evil-mode-p
+                      rime-predicate-punctuation-line-begin-p
+                      rime-predicate-auto-english-p))))
