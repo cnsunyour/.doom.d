1181077824c5b3d423394dbb0847b8c00a3cf212
Sunn Yao <sunyour@gmail.com>
2020-03-24 13:02:40 +0800

    Move back non-addition config.

--- modules/cnsunyour/telega/config.el
+++ modules/cnsunyour/telega/config.el
@@ -40,4 +40,24 @@
   (set-popup-rule! "^◀[[({<].+[\])}>]"
     :side 'right :size 100 :quit t :modeline t)
 
+  (telega-mode-line-mode 1)
+  (telega-url-shorten-mode 1)
+  (when (and IS-LINUX (boundp 'dbus-runtime-version))
+    (telega-notifications-mode 1))
+
+  (when (featurep! :completion ivy)
+    (load! "+ivy-telega"))
+
+  (after! all-the-icons
+    (add-to-list 'all-the-icons-mode-icon-alist
+                 '(telega-root-mode all-the-icons-fileicon "telegram"
+                                    :heigt 1.0
+                                    :v-adjust -0.2
+                                    :face all-the-icons-yellow))
+    (add-to-list 'all-the-icons-mode-icon-alist
+                 '(telega-chat-mode all-the-icons-fileicon "telegram"
+                                    :heigt 1.0
+                                    :v-adjust -0.2
+                                    :face all-the-icons-blue)))
+
   (load! "+telega-addition"))
