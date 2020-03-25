ced9fc2264e5e10888a94803d93db61193053894
Sunn Yao <sunyour@gmail.com>
2020-03-25 19:12:30 +0800

    Adjust the delay time.

--- modules/cnsunyour/ui/config.el
+++ modules/cnsunyour/ui/config.el
@@ -137,7 +137,7 @@
 ;; 只有放在module config.el files之后，doom-init-ui-hook之前才能正常执行
 (use-package! theme-changer
   :custom
-  (theme-changer-delay-seconds 2100 "Delay 35 minutes for sync with macOS's auto theme changer.")
+  (theme-changer-delay-seconds 1800 "Delay 30 minutes for sync with macOS's auto theme changer.")
   :hook
   ('emacs-startup . (lambda ()
                       (change-theme '(doom-one-light
