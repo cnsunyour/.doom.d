192b465f3b5e05374485c3825f6f1072d9ca72ca
Sunn Yao <sunyour@gmail.com>
2020-03-25 19:11:43 +0800

    Add some common functions.

--- config.el
+++ config.el
@@ -75,3 +75,25 @@
   "Deactive input method"
   (when (string= current-input-method default-input-method)
     (toggle-input-method)))
+
+(defun cnsunyour/string-match-p (regexp string &optional start)
+  "与 `string-match-p' 类似，如果 REGEXP 和 STRING 是非字符串时，
+不会报错。"
+  (and (stringp regexp)
+       (stringp string)
+       (string-match-p regexp string start)))
+
+(defun cnsunyour/char-before-to-string (num)
+  "得到光标前第 `num' 个字符，并将其转换为字符串。"
+  (let* ((point (point))
+         (point-before (- point num)))
+    (when (and (> point-before 0)
+               (char-before point-before))
+      (char-to-string (char-before point-before)))))
+
+(defun cnsunyour/char-after-to-string (num)
+  "得到光标后第 `num' 个字符，并将其转换为字符串。"
+  (let* ((point (point))
+         (point-after (+ point num)))
+    (when (char-after point-after)
+      (char-to-string (char-after point-after)))))
