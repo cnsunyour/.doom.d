af59a15f14968afa10f6e6130498ef3d497cbf0a
Sunn Yao <sunyour@gmail.com>
2020-03-27 00:50:50 +0800

    Modify doom-modeline to use `rime-lighter`

--- modules/cnsunyour/chinese/config.el
+++ modules/cnsunyour/chinese/config.el
@@ -115,6 +115,34 @@ unwanted space when exporting org-mode to hugo markdown."
                      (ignore-errors (rime-sync)))))
   ('doom-load-theme . #'+rime-auto-set-default-face)
   :config
+  (after! doom-modeline
+    (message "runing def-segment...")
+    (doom-modeline-def-segment input-method
+      "Define the current input method properties."
+      (propertize (cond (current-input-method
+                         (concat (doom-modeline-spc)
+                                 current-input-method-title
+                                 (doom-modeline-spc)))
+                        ((and (bound-and-true-p evil-local-mode)
+                              (bound-and-true-p evil-input-method))
+                         (concat
+                          (doom-modeline-spc)
+                          (nth 3 (assoc default-input-method input-method-alist))
+                          (doom-modeline-spc)))
+                        (t ""))
+                  'face (if (doom-modeline--active)
+                            (or (get-text-property 0 'face (rime-lighter))
+                                'doom-modeline-buffer-major-mode)
+                          'mode-line-inactive)
+                  'help-echo (concat
+                              "Current input method: "
+                              current-input-method
+                              "\n\
+mouse-2: Disable input method\n\
+mouse-3: Describe current input method")
+                  'mouse-face 'mode-line-highlight
+                  'local-map mode-line-input-method-map)))
+
   (defun +rime-auto-set-default-face ()
     "Set `rime-posframe-properties' according to the curremt emacs theme."
     (let* ((is-dark (eq (frame-parameter nil 'background-mode) 'dark))
