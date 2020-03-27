d72856953aadc80fa03f76f14f2bd8af38cf3aa0
Sunn Yao <sunyour@gmail.com>
2020-03-27 22:17:40 +0800

    Update format.

--- modules/cnsunyour/chinese/packages.el
+++ modules/cnsunyour/chinese/packages.el
@@ -5,8 +5,10 @@
 ;; (package! cnfonts)
 (package! pangu-spacing :disable t)
 (package! emacs-rime
-  :recipe (:host github :repo "DogLooksGood/emacs-rime" :files ("Makefile" "lib.c" "rime.el")))
-                 ;; :fork (:host github :repo "cnsunyour/emacs-rime")))
+  :recipe (:host github :repo "DogLooksGood/emacs-rime"
+                 ;; :fork (:host github :repo "cnsunyour/emacs-rime")
+                 ;; :local-repo (expand-file-name "~/repos/emacs-rime")
+                 :files ("Makefile" "lib.c" "rime.el")))
 (package! fcitx)
 (package! ace-pinyin)
 (package! pinyinlib)
