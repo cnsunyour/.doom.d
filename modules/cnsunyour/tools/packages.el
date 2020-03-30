50eaf08baa9462a50bb54df61cea789ac5c4c7f0
Sunn Yao <sunyour@gmail.com>
2020-03-30 19:51:55 +0800

    Enable snails package.

--- modules/cnsunyour/tools/packages.el
+++ modules/cnsunyour/tools/packages.el
@@ -13,6 +13,6 @@
 
 ;; 超强的搜索框架， exec-path-from-shell 和 fuz 是其需要的工具
 (package! exec-path-from-shell :disable t)
-(package! fuz :disable t)
-(package! snails :disable t
+(package! fuz)
+(package! snails
   :recipe (:host github :repo "manateelazycat/snails" :no-byte-compile t))
