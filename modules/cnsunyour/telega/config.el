e05ce1d56e4ad21abd02c6da7cd6697e8477fbf2
Sunn Yao <sunyour@gmail.com>
2020-04-01 23:31:40 +0800

    Use `visual-line-mode` instead.

--- modules/cnsunyour/telega/config.el
+++ modules/cnsunyour/telega/config.el
@@ -20,7 +20,7 @@
   (unless (display-graphic-p) (setq telega-use-images nil))
   :hook
   ('telega-chat-mode . #'yas-minor-mode-on)
-  ('telega-chat-mode . (lambda () (toggle-truncate-lines -1)))
+  ('telega-chat-mode . #'visual-line-mode)
   ('telega-chat-mode . (lambda ()
                          (set-company-backend! 'telega-chat-mode
                            (append '(telega-company-emoji
