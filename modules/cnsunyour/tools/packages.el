6822aedb4eb23545dd314866a68d9bc692ec288c
Sunn Yao <sunyour@gmail.com>
2020-04-21 00:52:20 +0800

    Remove `shrface` package.

--- modules/cnsunyour/tools/packages.el
+++ modules/cnsunyour/tools/packages.el
@@ -10,5 +10,3 @@
   :recipe (:host github :repo "manateelazycat/auto-save"))
 (package! neopastebin
   :recipe (:host github :repo "dhilst/emacs-pastebin" :fork "cnsunyour/emacs-pastebin"))
-(package! shrface
-  :recipe (:host github :repo "chenyanming/shrface"))
