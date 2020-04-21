6e34ab8f054276ac0b01ae2540877f7db8a927fd
Sunn Yao <sunyour@gmail.com>
2020-04-21 22:17:32 +0800

    Enable spell and grammer checkers

--- init.el
+++ init.el
@@ -76,8 +76,8 @@
 
        :checkers
        (syntax +childframe)              ; tasing you for every semicolon you forget
-       ;; spell             ; tasing you for misspelling mispelling
-       ;; grammar           ; tasing grammar mistake every you make
+       spell             ; tasing you for misspelling mispelling
+       grammar           ; tasing grammar mistake every you make
 
        :tools
        ;; ansible
