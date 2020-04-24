fc770b8586957970d8ca216d86b56a8941ded122
Sunn Yao <sunyour@gmail.com>
2020-04-24 17:37:32 +0800

    Update weechat config.

--- modules/cnsunyour/irc/config.el
+++ modules/cnsunyour/irc/config.el
@@ -6,11 +6,12 @@
   :commands (weechat-switch-buffer)
   :bind
   ("C-c C-b" . #'weechat-switch-buffer)
-  :init
-  (setq weechat-host-default "linode.sunyour.org"
-        weechat-port-default 29009
-        weechat-mode-default 'ssl
-        weechat-auto-monitor-buffers t)
+  :custom
+  (weechat-host-default "linode.sunyour.org")
+  (weechat-port-default 29009)
+  (weechat-mode-default 'ssl)
+  (weechat-auto-monitor-buffers t)
+  (weechat-notification-types 'weechat-sauron)
   :hook
   (weechat-mode . toggle-truncate-lines)
   :config
@@ -19,6 +20,8 @@
   (after! all-the-icons
     (add-to-list 'all-the-icons-mode-icon-alist
                  '(weechat-mode all-the-icons-fileicon "circle-ci"
-                                    :heigt 1.0
-                                    :v-adjust -0.2)))
-  (add-to-list 'weechat-modules 'weechat-image t))
+                                :heigt 1.0
+                                :v-adjust -0.2)))
+  (add-to-list 'weechat-modules 'weechat-image t)
+  (add-to-list 'weechat-modules 'weechat-sauron t)
+  (add-to-list 'weechat-modules 'weechat-tracking t))
