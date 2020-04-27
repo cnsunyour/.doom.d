ba0e083dd4bcbf1319f60967a25bbac9cfa5391f
Sunn Yao <sunyour@gmail.com>
2020-04-27 23:40:08 +0800

    Map key s-[n] to M-[n]

--- modules/cnsunyour/bindings/config.el
+++ modules/cnsunyour/bindings/config.el
@@ -4,6 +4,18 @@
   (setq mac-command-modifier 'super
         mac-option-modifier 'meta))
 
+(when (featurep! :ui workspaces)
+  (define-key key-translation-map (kbd "s-1") (kbd "M-1"))
+  (define-key key-translation-map (kbd "s-2") (kbd "M-2"))
+  (define-key key-translation-map (kbd "s-3") (kbd "M-3"))
+  (define-key key-translation-map (kbd "s-4") (kbd "M-4"))
+  (define-key key-translation-map (kbd "s-5") (kbd "M-5"))
+  (define-key key-translation-map (kbd "s-6") (kbd "M-6"))
+  (define-key key-translation-map (kbd "s-7") (kbd "M-7"))
+  (define-key key-translation-map (kbd "s-8") (kbd "M-8"))
+  (define-key key-translation-map (kbd "s-9") (kbd "M-9"))
+  (define-key key-translation-map (kbd "s-0") (kbd "M-0")))
+
 ;; general keybindings
 (map! (:leader
         (:prefix "b"
