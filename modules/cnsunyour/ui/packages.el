c3e6711f76f291e714d9f1557db4f471ae0388c8
Sunn Yao <sunyour@gmail.com>
2020-05-05 07:54:10 +0800

    Add package origin repo

--- modules/cnsunyour/ui/packages.el
+++ modules/cnsunyour/ui/packages.el
@@ -4,7 +4,7 @@
 
 (when IS-MAC
   (package! auto-dark-emacs
-    :recipe (:host github :repo "cnsunyour/auto-dark-emacs")))
+    :recipe (:host github :repo "LionyxML/auto-dark-emacs" :fork "cnsunyour/auto-dark-emacs")))
 (package! awesome-tab
   :recipe (:host github :repo "manateelazycat/awesome-tab"))
 (package! emojify)
