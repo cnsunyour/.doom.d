612106f6e05c45f7169652488c739fc741727efa
Sunn Yao <sunyour@gmail.com>
2020-05-08 12:07:07 +0800

    Don't enable lsp on "sh"/"web" module

--- init.el
+++ init.el
@@ -154,11 +154,11 @@
        ;;ruby            ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (sh +lsp)                ; she sells {ba,z,fi}sh shells on the C xor
+       sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       (web +lsp)               ; the tubes
+       web               ; the tubes
        ;;vala              ; GObjective-C
 
        :email
