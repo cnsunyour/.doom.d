c7a9bb4e657a30c90a5bf1f81a1514ed4398b297
Sunn Yao <sunyour@gmail.com>
2020-06-29 04:41:08 +0800

    更新一下格式

--- modules/cnsunyour/tools/packages.el
+++ modules/cnsunyour/tools/packages.el
@@ -9,6 +9,7 @@
 (package! auto-save
   :recipe (:host github :repo "manateelazycat/auto-save"))
 (package! neopastebin
-  :recipe (:host github :repo "dhilst/emacs-pastebin" :fork "cnsunyour/emacs-pastebin"))
+  :recipe (:host github :repo "dhilst/emacs-pastebin"
+           :fork "cnsunyour/emacs-pastebin"))
 (package! keyfreq
   :recipe (:host github :repo "dacap/keyfreq"))
