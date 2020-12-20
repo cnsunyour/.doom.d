cee9d9e793da26c4985c4becf822d8c40b50aa5d
Sunn Yao <sunyour@gmail.com>
2020-12-20 08:08:17 +0800

    不启用 ligatures

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
-       ligatures         ; ligatures and symbols to make your code pretty again
+       ;; ligatures         ; ligatures and symbols to make your code pretty again
        ;; tabs
        treemacs          ; a project drawer, like neotree but cooler
        ;; unicode           ; extended unicode support for various languages
