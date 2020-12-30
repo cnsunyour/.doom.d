99a06aaf4aa1aef64d3f6f6d731662d7ba1a42aa
Sunn Yao <sunyour@gmail.com>
2020-12-30 09:52:16 +0800

    Temporary solution for 'tkk fail'

--- modules/cnsunyour/translate/config.el
+++ modules/cnsunyour/translate/config.el
@@ -107,6 +107,10 @@
         "T" #'google-translate-chinese-at-point)
 
   :config
+  (defun google-translate--get-b-d1 ()
+    ;; TKK='427110.1469889687'
+    (list 427110 1477889687))
+
   (set-popup-rule! "^\\*Google Translate\\*" :side 'right :size 0.4 :select t)
 
   (defcustom google-translate-tooltip-name "*google-translate-posframe*"
