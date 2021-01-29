5c423986ac3f991cb637a1d156c6b2834839d533
Sunn Yao <sunyour@gmail.com>
2021-01-29 13:47:02 +0800

    update `auto-dark-emacs` version

--- modules/cnsunyour/ui/packages.el
+++ modules/cnsunyour/ui/packages.el
@@ -7,7 +7,7 @@
     :recipe (:host github :repo "cnsunyour/theme-changer")))
 (when IS-MAC
   (package! auto-dark-emacs
-    :pin "6e6debf4e580984eaa4986ab557df2511546c333"
+    :pin "fede6f4c7ae87a0abc50591e2b29ed0666c1522e"
     :recipe (:host github :repo "cnsunyour/auto-dark-emacs")))
 (package! awesome-tab
   :pin "5f2c76d5f889991baca6ee8e823c1c9dceeec05b"
