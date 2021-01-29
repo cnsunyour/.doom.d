7b55d84431d3a5490aeed069bd643f4fc9878ee1
Sunn Yao <sunyour@gmail.com>
2021-01-29 21:07:15 +0800

    rename argument name

--- init.el
+++ init.el
@@ -9,11 +9,11 @@
 ;; found in modules/README.org.
 
 (when (version< "27.1" emacs-version)
-  (define-advice define-obsolete-function-alias (:filter-args (ll) fix-obsolete)
-    (let ((obsolete-name (pop ll))
-          (current-name (pop ll))
-          (when (if ll (pop ll) "1"))
-          (docstring (if ll (pop ll) nil)))
+  (define-advice define-obsolete-function-alias (:filter-args (args) fix-obsolete)
+    (let ((obsolete-name (pop args))
+          (current-name (pop args))
+          (when (if args (pop args) "1"))
+          (docstring (if args (pop args) nil)))
       (list obsolete-name current-name when docstring))))
 
 ;; Limit on depth in eval, apply and funcall before error
