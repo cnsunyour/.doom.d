677e5e457faa5a5395abbb3696be1fe0e53547ca
Sunn Yao <sunyour@gmail.com>
2021-01-31 14:52:13 +0800

    optimize +ivy-telega

--- modules/cnsunyour/telega/+ivy-telega.el
+++ modules/cnsunyour/telega/+ivy-telega.el
@@ -6,21 +6,33 @@
         (mentions (funcall (telega--tl-prop :unread_mention_count) chat)))
 
     (if (and not-muted-p (> (+ unread mentions) 0))
-        (ivy-append-face (format "%s %d@%d" title unread mentions) 'ivy-highlight-face)
+        (concat (ivy-append-face (format "%s(" title) 'ivy-highlight-face)
+                (ivy-append-face (format "%d" unread) 'telega-unmuted-count)
+                (when (> mentions 0)
+                  (ivy-append-face (format "@%d" mentions) 'telega-mention-count))
+                (ivy-append-face ")" 'ivy-highlight-face))
       title)))
 
 ;;;###autoload
-(defun ivy-telega-chat-with ()
+(defun ivy-telega-chat-with (&optional prefix-u)
   "Starts chat with defined peer"
-  (interactive)
+  (interactive "P")
 
   (telega t)
   (let ((chats (mapcar
                 (lambda (x) (cons (ivy-telega-chat-highlight x) x))
-                (telega-filter-chats telega--ordered-chats 'all))))
-    (ivy-read "Chat with: " chats
-              :action (lambda (x) (telega-chat--pop-to-buffer (cdr x)))
-              :caller 'ivy-telega-chat-with)))
+                (telega-filter-chats telega--ordered-chats
+                                     (if prefix-u
+                                         'all
+                                       '(or mention (and unread unmuted)))))))
+    (cond ((null chats)
+           (user-error "No chats"))
+          ((= 1 (length chats))
+           (telega-chat--pop-to-buffer (cdar chats)))
+          (t
+           (ivy-read "Chat with: " chats
+                     :action (lambda (x) (telega-chat--pop-to-buffer (cdr x)))
+                     :caller 'ivy-telega-chat-with)))))
 
 (setq telega-completing-read-function 'ivy-completing-read)
 
@@ -28,4 +40,5 @@
   (push '(telega-chat-with . ivy--regex-pinyin) ivy-re-builders-alist)
   (push '(ivy-telega-chat-with . ivy--regex-pinyin) ivy-re-builders-alist))
 
-(map! "C-c v" #'ivy-telega-chat-with)
+(map! "C-c v" #'ivy-telega-chat-with
+      :leader "v" #'ivy-telega-chat-with)
