8c1518bd99fe3a478c79268449617c6695090256
Sunn Yao <sunyour@gmail.com>
2021-03-21 11:07:36 +0800

    switch command and option key

--- modules/cnsunyour/bindings/config.el
+++ modules/cnsunyour/bindings/config.el
@@ -1,22 +1,23 @@
 ;;;  -*- lexical-binding: t; -*-
 
 (when IS-MAC
-  (setq mac-command-modifier 'super
-        mac-option-modifier 'meta))
+  (setq mac-command-modifier 'meta
+        mac-option-modifier 'super))
 
-(when (featurep! :ui workspaces)
-  (global-set-key (kbd "s-[") #'+workspace/switch-left)
-  (global-set-key (kbd "s-]") #'+workspace/switch-right)
-  (define-key key-translation-map (kbd "s-1") (kbd "M-1"))
-  (define-key key-translation-map (kbd "s-2") (kbd "M-2"))
-  (define-key key-translation-map (kbd "s-3") (kbd "M-3"))
-  (define-key key-translation-map (kbd "s-4") (kbd "M-4"))
-  (define-key key-translation-map (kbd "s-5") (kbd "M-5"))
-  (define-key key-translation-map (kbd "s-6") (kbd "M-6"))
-  (define-key key-translation-map (kbd "s-7") (kbd "M-7"))
-  (define-key key-translation-map (kbd "s-8") (kbd "M-8"))
-  (define-key key-translation-map (kbd "s-9") (kbd "M-9"))
-  (define-key key-translation-map (kbd "s-0") (kbd "M-0")))
+;; (when (featurep! :ui workspaces)
+;;   (global-set-key (kbd "s-[") #'+workspace/switch-left)
+;;   (global-set-key (kbd "s-]") #'+workspace/switch-right)
+;;   (define-key key-translation-map (kbd "s-1") (kbd "M-1"))
+;;   (define-key key-translation-map (kbd "s-2") (kbd "M-2"))
+;;   (define-key key-translation-map (kbd "s-3") (kbd "M-3"))
+;;   (define-key key-translation-map (kbd "s-4") (kbd "M-4"))
+;;   (define-key key-translation-map (kbd "s-5") (kbd "M-5"))
+;;   (define-key key-translation-map (kbd "s-6") (kbd "M-6"))
+;;   (define-key key-translation-map (kbd "s-7") (kbd "M-7"))
+;;   (define-key key-translation-map (kbd "s-8") (kbd "M-8"))
+;;   (define-key key-translation-map (kbd "s-9") (kbd "M-9"))
+;;   (define-key key-translation-map (kbd "s-0") (kbd "M-0")))
+
 
 ;; general keybindings
 (map! (:leader (:prefix "b" "h" #'+doom-dashboard/open))
