9abdb0dc1123f797dd34fed857264b611541404f
Sunn Yao <sunyour@gmail.com>
2021-06-21 02:50:33 +0800

    update a keybinding

--- modules/cnsunyour/bindings/config.el
+++ modules/cnsunyour/bindings/config.el
@@ -23,7 +23,7 @@
 (map! (:leader (:prefix "b" "h" #'+doom-dashboard/open))
 
       :g "C-!" #'kill-buffer-and-window
-      :g "C-M-S-l" #'toggle-truncate-lines
+      :g "C-s-S-l" #'toggle-truncate-lines
       :g "M-o" #'evil-window-next
       :g "M-O" #'evil-window-prev
 
