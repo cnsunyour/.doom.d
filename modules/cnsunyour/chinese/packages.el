6f0aad001bb28187c00b49830add26ca483483db
Sunn Yao <sunyour@gmail.com>
2021-08-10 11:34:53 +0800

    chore: 去除第三方包的 pin 参数

--- modules/cnsunyour/chinese/packages.el
+++ modules/cnsunyour/chinese/packages.el
@@ -2,11 +2,12 @@
 ;;; cnsunyour/chinese/packages.el
 
 
-(package! rime)
+(package! rime
   ;; :recipe (:host github :repo "DogLooksGood/emacs-rime"
   ;;                ;; :fork "cnsunyour/emacs-rime"
   ;;                ;; :local-repo "~/repos/emacs-rime"
-  ;;                :files ("Makefile" "lib.c" "*.el")))
-(package! fcitx :pin "12dc2638ddd15c8f6cfaecb20e1f428ab2bb5624")
-(package! ace-pinyin :pin "8b2e9335b02486730ea4ceee790130cc5328f9ea")
-(package! pinyinlib :pin "1772c79b6f319b26b6a394a8dda065be3ea4498d")
+  ;;                :files ("Makefile" "lib.c" "*.el"))
+  )
+(package! fcitx)
+(package! ace-pinyin)
+(package! pinyinlib)
--- modules/cnsunyour/ui/packages.el
+++ modules/cnsunyour/ui/packages.el
@@ -10,6 +10,5 @@
     :pin "e4d20c3b0e6d83101d615807e0adc0203ebb1bc7"
     :recipe (:host github :repo "cnsunyour/auto-dark-emacs")))
 (package! awesome-tab
-  :pin "5f2c76d5f889991baca6ee8e823c1c9dceeec05b"
   :recipe (:host github :repo "manateelazycat/awesome-tab"))
 (package! emojify)
