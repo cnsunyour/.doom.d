54ed177d8f2d4ad6db0bd10ac4cce2fe7045e0f6
Sunn Yao <sunyour@gmail.com>
2021-10-29 16:17:13 +0800

    disable emojify package

--- modules/cnsunyour/ui/packages.el
+++ modules/cnsunyour/ui/packages.el
@@ -9,4 +9,4 @@
   (package! auto-dark-emacs
     :pin "e4d20c3b0e6d83101d615807e0adc0203ebb1bc7"
     :recipe (:host github :repo "cnsunyour/auto-dark-emacs")))
-(package! emojify)
+(package! emojify :disable t)
