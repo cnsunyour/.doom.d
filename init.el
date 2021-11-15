0fbe2439c2d10c207e7db9682b502e65e8b046ba
Sunn Yao <sunyour@gmail.com>
2021-11-15 21:23:20 +0800

    fix: parinfer use `parinfer-rust-mode` only

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
        ;;lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       (parinfer +rust)          ; turn lisp into python, sort of
+       parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;; word-wrap
