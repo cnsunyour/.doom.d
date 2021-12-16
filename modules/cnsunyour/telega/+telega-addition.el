79f813deaf6034f1cdfc1d4e5bca08df24716089
Sunn Yao <sunyour@gmail.com>
2021-12-17 00:32:41 +0800

    style(telega): update emoji config

--- modules/cnsunyour/telega/+telega-addition.el
+++ modules/cnsunyour/telega/+telega-addition.el
@@ -30,12 +30,12 @@
             (list 'all               "[" "]")))
 
 
-(make-face 'my-align-by-sarasa)
-(set-face-font 'my-align-by-sarasa (font-spec :family "Sarasa Mono SC"))
-(defun my-align-with-sarasa-font ()
-  (interactive)
-  (when (member "Sarasa Mono SC" (font-family-list))
-    (set (make-variable-buffer-local 'buffer-face-mode-face) 'my-align-by-sarasa)
+(when (display-graphic-p)
+  (make-face 'my-align-for-font)
+  (set-face-font 'my-align-for-font doom-font)
+  (defun my-align-with-sarasa-font ()
+    (interactive)
+    (set (make-variable-buffer-local 'buffer-face-mode-face) 'my-align-for-font)
     (make-variable-buffer-local 'face-font-rescale-alist)
     ;; make symbols smaller
     (add-to-list 'face-font-rescale-alist '("-Noto Color Emoji-" . 0.8))
@@ -43,6 +43,6 @@
     (add-to-list 'face-font-rescale-alist '("-Noto Sans Symbols-" . 0.8))
     (add-to-list 'face-font-rescale-alist '("-Noto Sans Symbols2-" . 0.8))
     (add-to-list 'face-font-rescale-alist '("-Symbola-" . 0.8))
-    (buffer-face-mode 1)))
-(add-hook! '(telega-root-mode-hook telega-chat-mode-hook)
-           #'my-align-with-sarasa-font)
+    (buffer-face-mode 1))
+  (add-hook! '(telega-root-mode-hook telega-chat-mode-hook)
+             #'my-align-with-sarasa-font))
