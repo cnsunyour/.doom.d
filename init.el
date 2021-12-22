5e7cbbc5698bc92f20bb2a790f6929512906d676
Sunn Yao <sunyour@gmail.com>
2021-12-23 01:15:25 +0800

    enable editorconfig module

--- init.el
+++ init.el
@@ -84,7 +84,7 @@
        ;; debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        docker
-       ;; editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        gist              ; interacting with github gists
