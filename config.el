e0b76cb7ec6b9966e7d1d931001d736ffe091aa0
Sunn Yao <sunyour@gmail.com>
2022-01-09 00:01:54 +0800

    set intelephense licekse key

--- config.el
+++ config.el
@@ -89,3 +89,8 @@
 
 (when (featurep! :app rss)
   (add-hook! 'elfeed-search-mode-hook 'elfeed-update))
+
+(after! php-mode
+  (setq lsp-intelephense-licence-key
+        (auth-source-pick-first-password
+         :host "intelephense")))
