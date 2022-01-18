58b20f7482f708178145049463f337c90540b6b1
Sunn Yao <sunyour@gmail.com>
2022-01-19 05:51:11 +0800

    remove key binding

--- modules/cnsunyour/beancount/config.el
+++ modules/cnsunyour/beancount/config.el
@@ -4,8 +4,6 @@
 ;; beancount复式账簿记账
 (use-package! beancount
   :defer t
-  :bind
-  ("C-M-S-s-b" . (lambda() (interactive) (find-file "~/Dropbox/beancount/main.bean")))
   :mode
   ("\\.bean\\(?:count\\)?\\'" . beancount-mode)
   :hook
