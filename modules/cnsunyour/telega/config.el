58cb3389e5527347eea1e9bb64223347f2c6bc7e
Sunn Yao <sunyour@gmail.com>
2022-07-17 02:00:47 +0800

    chore: update telega popup params

--- modules/cnsunyour/telega/config.el
+++ modules/cnsunyour/telega/config.el
@@ -67,8 +67,8 @@
     'emacs)
 
   (set-popup-rule! (regexp-quote telega-root-buffer-name)
-    :side 'right :size 100 :select t :ttl t :quit t :modeline t)
+    :side 'right :size 100 :select t :ttl nil :quit t :modeline t)
   (set-popup-rule! "^◀[^◀\[]*[\[({<].+[\])}>]"
-    :side 'right :size 100 :select t :ttl t :quit t :modeline t)
+    :side 'right :size 100 :select t :ttl 010 :quit t :modeline t)
 
   (load! "+telega-addition"))
