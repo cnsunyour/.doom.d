d07d5327943333db7a91b21d81df31f05fd8c4f6
Sunn Yao <sunyour@gmail.com>
2022-07-26 04:55:04 +0800

    setup apikey&secretkey for youdao-dictionary

--- modules/cnsunyour/translate/config.el
+++ modules/cnsunyour/translate/config.el
@@ -69,9 +69,9 @@
   :config
   (set-popup-rule! (regexp-quote youdao-dictionary-buffer-name) :side 'right :size 0.5 :select t)
   ;; 设定youdao api id和key
-  ;; (let ((credentials (auth-source-user-and-password "youdao-api")))
-  ;;   (setq youdao-dictionary-app-key (car credentials)
-  ;;         youdao-dictionary-secret-key (cadr credentials)))
+  (let ((credentials (auth-source-user-and-password "youdao-api")))
+    (setq youdao-dictionary-app-key (car credentials)
+          youdao-dictionary-secret-key (cadr credentials)))
   ;; Enable Cache
   (setq url-automatic-caching t)
   ;; Enable Chinese word segmentation support (支持中文分词)
