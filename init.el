6358b34cf2dd79cfebca36d6e8c04c216f95159e
Sunn Yao <sunyour@gmail.com>
2022-07-26 04:56:07 +0800

    enable lua module

--- init.el
+++ init.el
@@ -134,7 +134,7 @@
         +cdlatex
         +lsp)             ; writing papers in Emacs has never been so fun
        ;;ledger            ; an accounting system in Emacs
-       ;;lua               ; one-based indices? one-based indices
+       (lua +lsp)               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
