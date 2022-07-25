dc50705b900172d90471f67f5a09d6983b338fa3
Sunn Yao <sunyour@gmail.com>
2022-07-26 05:36:25 +0800

    Redo "remove unused packages"
    This reverts commit d99a5741a2e486b6361155bfb2815afe1353e40f.

--- modules/cnsunyour/translate/packages.el
+++ modules/cnsunyour/translate/packages.el
@@ -1,13 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; cnsunyour/translate/packages.el
 
-
-(package! company-english-helper
-  :recipe (:host github :repo "manateelazycat/company-english-helper"))
-(package! insert-translated-name
-  :recipe (:host github :repo "manateelazycat/insert-translated-name"))
-(package! sdcv
-  :recipe (:host github :repo "manateelazycat/sdcv"))
 (package! youdao-dictionary)
 (package! fanyi)
 (package! go-translate)
