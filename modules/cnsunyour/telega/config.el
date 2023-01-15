f6dc24790e54c4133a09b29747e5613f17b1a7c3
Sunn Yao <sunyour@gmail.com>
2023-01-15 23:58:45 +0800

    don't ignore messages by blocked users

--- modules/cnsunyour/telega/config.el
+++ modules/cnsunyour/telega/config.el
@@ -26,8 +26,8 @@
   (telega-load . telega-autoplay-mode)
 
   :config
-  (add-hook 'telega-msg-ignore-predicates
-            (telega-match-gen-predicate "msg-" '(sender blocked)))
+  ;; (add-hook 'telega-msg-ignore-predicates
+  ;;           (telega-match-gen-predicate "msg-" '(sender blocked)))
   ;; (setq telega-proxies (list '(:server "127.0.0.1" :port 1086 :enable t
   ;;                         :type (:@type "proxyTypeSocks5"))))
   (setq telega-chat-show-deleted-messages-for '(me-is-owner OR-ADMIN)
