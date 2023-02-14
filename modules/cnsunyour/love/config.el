330a9fdbdc85a4df42c9ed7461f504babafdd910
Sunn Yao <sunyour@gmail.com>
2023-02-15 07:18:20 +0800

    update love-live-time

--- modules/cnsunyour/love/config.el
+++ modules/cnsunyour/love/config.el
@@ -7,14 +7,10 @@
          (live-time  (time-subtract (current-time) birth-time))
          (lt-secs    (float-time live-time)))
     (message
-     (format "%s: %d days; %.2f months; %.2f weeks; -- %s"
+     (format "【%s】-> %d天 | %.2f月 | %.2f周 | %s"
              name
              (floor (/ lt-secs 86400))
              (/ lt-secs 2628000) ;; 1 y = 12 m, 1 m ~= 30.4166667 d
              (/ lt-secs 604800)
-             (format-seconds "%Y, %D, %H, %M%z" lt-secs)))))
+             (format-seconds "%y年%d天%h时%m分%z" lt-secs)))))
 
-(map! :leader
-      (:prefix-map ("k" . "Lover's live time")
-        :desc "Twinkle's live time" "k" (cmd! (cnsunyour/lover-live-time "Twinkle" 2013 9 16 13 43 0))
-        :desc "Marry passed time" "m" (cmd! (cnsunyour/lover-live-time "Married" 2002 10 4 9 0 0))))
