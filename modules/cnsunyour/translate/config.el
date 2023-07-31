d408f913df1b0b354292ca370f5a2af06604326a
Sunn Yao <sunyour@gmail.com>
2023-08-01 00:59:12 +0800

    remove youdao-distionary package

--- modules/cnsunyour/translate/config.el
+++ modules/cnsunyour/translate/config.el
@@ -1,26 +1,5 @@
 ;;; ~/.doom.d/+translate.el -*- lexical-binding: t; -*-
 
-;; 有道词典
-(use-package! youdao-dictionary
-  :defer t
-  :init
-  (map! :leader
-        :prefix ("y" . "Translate")
-        "y" #'youdao-dictionary-search-at-point-posframe
-        "Y" #'youdao-dictionary-search-at-point
-        "p" #'youdao-dictionary-play-voice-at-point
-        "P" #'youdao-dictionary-play-voice-from-input)
-  :config
-  (set-popup-rule! (regexp-quote youdao-dictionary-buffer-name) :side 'right :size 80 :select t :quit t)
-  ;; 设定youdao api id和key
-  (let ((credentials (auth-source-user-and-password "youdao-api")))
-    (setq youdao-dictionary-app-key (car credentials)
-          youdao-dictionary-secret-key (cadr credentials)))
-  ;; Enable Cache
-  (setq url-automatic-caching t)
-  ;; Enable Chinese word segmentation support (支持中文分词)
-  (setq youdao-dictionary-use-chinese-word-segmentation t))
-
 
 (use-package! fanyi
   :defer t
--- modules/cnsunyour/translate/packages.el
+++ modules/cnsunyour/translate/packages.el
@@ -1,6 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; cnsunyour/translate/packages.el
 
-(package! youdao-dictionary)
 (package! fanyi)
 (package! go-translate)
