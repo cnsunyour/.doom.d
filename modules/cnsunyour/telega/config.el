00b93525f6a5f4a1c8b21fdd17798f6ad9900b0f
Sunn Yao <sunyour@gmail.com>
2023-11-18 18:08:36 +0800

    update telega config

--- modules/cnsunyour/telega/config.el
+++ modules/cnsunyour/telega/config.el
@@ -29,9 +29,26 @@
         telega-sticker-size '(8 . 48)
         telega-translate-to-language-by-default "zh"
         telega-avatar-workaround-gaps-for `(return t)
+        telega-symbols-emojify (assq-delete-all 'vertical-bar telega-symbols-emojify)
+        telega-symbols-emojify (assq-delete-all 'checkmark telega-symbols-emojify)
+        telega-symbols-emojify (assq-delete-all 'heavy-checkmark telega-symbols-emojify)
+        telega-symbol-reply (nerd-icons-faicon "nf-fa-reply")
+        telega-symbol-reply-quote (nerd-icons-faicon "nf-fa-reply_all")
+        telega-symbol-forward (nerd-icons-mdicon "nf-md-comment_arrow_right_outline")
+        telega-symbol-checkmark (nerd-icons-codicon "nf-cod-check")
+        telega-symbol-heavy-checkmark (nerd-icons-codicon "nf-cod-check_all")
+        telega-symbol-right-arrow (nerd-icons-codicon "nf-cod-arrow_right")
+        telega-symbol-reaction (nerd-icons-mdicon "nf-md-heart_circle")
         telega-mode-line-string-format (remove
                                         '(:eval (telega-mode-line-icon))
                                         telega-mode-line-string-format))
+
+  (add-to-list 'telega-url-shorten-regexps
+               (list `(too-long-link
+                       :regexp "^\\(https?://\\)\\(.\\{60\\}\\).*?$"
+                       :symbol ""
+                       :replace "\\1\\2..."))
+               t)
 
   (map! :desc "Telega all chats"
         "C-c c" #'telega-chat-with
