5c674a0abf33cbd35d7fb70485e9456181689957
Sunn Yao <sunyour@gmail.com>
2023-11-22 00:22:16 +0800

    update telega config

--- modules/cnsunyour/telega/config.el
+++ modules/cnsunyour/telega/config.el
@@ -67,8 +67,6 @@
     :slot 10 :vslot 10 :side 'right :size (+ telega-chat-fill-column 11) :ttl nil :quit 'current :modeline t)
   (set-popup-rule! "^◀[^◀\[]*[\[({<].+[\])}>]"
     :slot 10 :vslot 10 :side 'right :size (+ telega-chat-fill-column 11) :ttl 10 :quit 'current :modeline t)
-  (set-popup-rule! (regexp-quote "*Telega Instant View*")
-    :slot 20 :vslot 10 :side 'right :height .5 :ttl 10 :quit t :modeline nil :select t)
   (set-popup-rule! (regexp-quote "*Telega User*")
     :slot 20 :vslot 10 :side 'right :height .5 :ttl 10 :quit t :modeline nil :select t)
   (set-popup-rule! (regexp-quote "*Telegram Chat Info*")
