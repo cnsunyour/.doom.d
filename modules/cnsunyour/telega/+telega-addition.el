279b1bdc894783dd037daa8205349452286b350d
Sunn Yao <sunyour@gmail.com>
2023-11-25 21:31:16 +0800

    telega: remove unused variable

--- modules/cnsunyour/telega/+telega-addition.el
+++ modules/cnsunyour/telega/+telega-addition.el
@@ -1,4 +1,4 @@
-;;; cnsunyour/telega/+telega-ext.el -*- lexical-binding: t; -*-
+;;; cnsunyour/telega/+telega-addition.el -*- lexical-binding: t; -*-
 
 ;; Highlight current line in root buffer
 (defun lg-telega-root-mode ()
@@ -6,28 +6,8 @@
 (defun lg-telega-chat-update (chat)
   (with-telega-root-buffer
     (hl-line-highlight)))
-(add-hook 'telega-chat-update-hook 'lg-telega-chat-update)
 (add-hook 'telega-root-mode-hook 'lg-telega-root-mode)
-
-;; Outline unmuted chats using braces
-(setq telega-chat-button-brackets
-      (list (list '(and (not unmuted) (type private))
-                  (propertize "{" 'face 'shadow)
-                  (propertize "}" 'face 'shadow))
-            (list '(and (not unmuted) (type basicgroup))
-                  (propertize "(" 'face 'shadow)
-                  (propertize ")" 'face 'shadow))
-            (list '(and (not unmuted) (type supergroup))
-                  (propertize "[" 'face 'shadow)
-                  (propertize "]" 'face 'shadow))
-            (list '(and (not unmuted) (type channel))
-                  (propertize "<" 'face 'shadow)
-                  (propertize ">" 'face 'shadow))
-            (list '(type private)    "{" "}")
-            (list '(type basicgroup) "(" ")")
-            (list '(type supergroup) "[" "]")
-            (list '(type channel)    "<" ">")
-            (list 'all               "[" "]")))
+(add-hook 'telega-chat-update-hook 'lg-telega-chat-update)
 
 ;; customize some icons/stickers/urls
 (setq telega-sticker-size '(8 . 48)
