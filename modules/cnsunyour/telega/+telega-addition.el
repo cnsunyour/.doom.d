50e0ac94ce3c62d03a278a62b884a13d1d02502f
Sunn Yao <sunyour@gmail.com>
2023-11-25 18:46:07 +0800

    add bilibili rule to shorten url

--- modules/cnsunyour/telega/+telega-addition.el
+++ modules/cnsunyour/telega/+telega-addition.el
@@ -43,42 +43,67 @@
 
 ;; add some rules to shorten urls
 (add-to-list 'telega-url-shorten-regexps
+             `(bilibili
+               :regexp "^https?://\\(?:\\w+\\.\\)?bilibili.com/\\(.+\\)\\?.*$"
+               :symbol #("󰷝" 0 1
+                         (face
+                          (:family "FontAwesome" :height 1.2)
+                          font-lock-face
+                          (:family "FontAwesome" :height 1.2)
+                          display
+                          (raise -0.24)
+                          rear-nonsticky t))
+               :replace "B站#\\1")
+             t)
+(add-to-list 'telega-url-shorten-regexps
              `(reddit
                :regexp "^https?://\\(?:www\\.\\)?reddit.com/\\(.+\\)/\\(.*\\)"
                :symbol #("" 0 1
-                         (face (:family "FontAwesome" :height 1.2)
-                               font-lock-face (:family "FontAwesome" :height 1.2)
-                               display (raise -0.24)
-                               rear-nonsticky t))
+                         (face
+                          (:family "FontAwesome" :height 1.2)
+                          font-lock-face
+                          (:family "FontAwesome" :height 1.2)
+                          display
+                          (raise -0.24)
+                          rear-nonsticky t))
                :replace "\\1/\\2")
              t)
 (add-to-list 'telega-url-shorten-regexps
              `(redd.it
                :regexp "^https?://\\(?:[a-zA-Z0-9-_]+\\.\\)?redd.it/\\(.*\\)\\?.*$"
                :symbol #("󰑍" 0 1
-                         (face (:family "FontAwesome" :height 1.2)
-                               font-lock-face (:family "FontAwesome" :height 1.2)
-                               display (raise -0.24)
-                               rear-nonsticky t))
+                         (face
+                          (:family "FontAwesome" :height 1.2)
+                          font-lock-face
+                          (:family "FontAwesome" :height 1.2)
+                          display
+                          (raise -0.24)
+                          rear-nonsticky t))
                :replace "\\1")
              t)
 (add-to-list 'telega-url-shorten-regexps
              `(emacs-china
                :regexp "^https?://\\(?:www\\.\\)?emacs-china.org/\\(.+\\)/\\(.*\\)"
                :symbol #("" 0 1
-                         (face (:family "FontAwesome" :height 1.2)
-                               font-lock-face (:family "FontAwesome" :height 1.2)
-                               display (raise -0.24)
-                               rear-nonsticky t))
+                         (face
+                          (:family "FontAwesome" :height 1.2)
+                          font-lock-face
+                          (:family "FontAwesome" :height 1.2)
+                          display
+                          (raise -0.24)
+                          rear-nonsticky t))
                :replace "\\1/\\2")
              t)
 (add-to-list 'telega-url-shorten-regexps
              `(too-long-link
                :regexp "^https?://\\(.\\{60\\}\\).*?$"
                :symbol #("" 0 1
-                         (face (:family "FontAwesome" :height 1.2)
-                               font-lock-face (:family "FontAwesome" :height 1.2)
-                               display (raise -0.24)
-                               rear-nonsticky t))
+                         (face
+                          (:family "FontAwesome" :height 1.2)
+                          font-lock-face
+                          (:family "FontAwesome" :height 1.2)
+                          display
+                          (raise -0.24)
+                          rear-nonsticky t))
                :replace "\\1...")
              t)
