b40937ef61ae7817e906f7f113ff746651a36c73
Sunn Yao <sunyour@gmail.com>
2024-01-07 03:57:01 +0800

    fix: blamer-face config

--- modules/cnsunyour/tools/config.el
+++ modules/cnsunyour/tools/config.el
@@ -103,8 +103,8 @@
   (blamer-min-offset 70)
   :custom-face
   (blamer-face ((t :foreground "#7a88cf"
-                    :background nil
-                    :height 140
-                    :italic t)))
+                   :background unspecified
+                   :height 140
+                   :italic t)))
   :config
   (global-blamer-mode 1))
