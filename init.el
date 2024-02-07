554e5d323e30615fefa3009e709c0e2af067e726
Sunn Yao <sunyour@gmail.com>
2024-02-07 15:58:36 +0800

    'gist' module removed

--- init.el
+++ init.el
@@ -86,7 +86,6 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       gist              ; interacting with github gists
        (lookup +dictionary +docsets)
        (lsp +peek)
        tree-sitter
