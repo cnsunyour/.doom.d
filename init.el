28d630e11257268bc7ebfd2dab76aa26dc96e3a2
Sunn Yao <sunyour@gmail.com>
2024-07-02 21:09:20 +0800

    remove unused module

--- init.el
+++ init.el
@@ -94,7 +94,6 @@
        (pass +auth)              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       rgb               ; creating color strings
        ;;terraform         ; infrastructure as code
        ;; tmux              ; an API for interacting with tmux
        upload            ; map local to remote projects via ssh/ftp
