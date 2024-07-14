ad2ff45dd2df577377ac2dc256b19ccc54dad692
Sunn Yao <sunyour@gmail.com>
2024-07-14 22:08:02 +0800

    Simplify translation taker and engine configuration

--- modules/cnsunyour/translate/packages.el
+++ modules/cnsunyour/translate/packages.el
@@ -2,6 +2,7 @@
 ;;; cnsunyour/translate/packages.el
 
 (package! go-translate
+  :pin "c8abe6793793401a4ec3c45553bcd75b518ecc78"
   :recipe (:host github
            :fork "cnsunyour"
            :repo "lorniu/go-translate"))
