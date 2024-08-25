d741d72712e1cfb3a205da20481ca8a378bea51f
Sunn Yao <sunyour@gmail.com>
2024-08-25 17:41:32 +0800

    move some private config to custom.el

--- config.el
+++ config.el
@@ -1,23 +1,13 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-(setq user-full-name "Sunn Yao"
-      frame-title-format (concat "%b - " user-full-name "'s Emacs")
-      user-mail-address "sunyour@gmail.com"
-      epa-file-encrypt-to user-mail-address)
+(setq epa-file-encrypt-to user-mail-address)
 
 ;; System locale to use for formatting time values.
 ;;
 ;; Make sure that the weekdays in the time stamps of your Org mode files and in
 ;; the agenda appear in English.
 (setq system-time-locale "C")
-
-;; 设置我所在地方的经纬度，calendar里有个功能是日月食的预测，和经纬度相联系的。
-;; 让emacs能计算日出日落的时间，在 calendar 上用 S 即可看到
-;; 另外根据日出日落时间切换主题也需要经纬度
-(setq calendar-location-name "Beijing, China")
-(setq calendar-latitude +39.9055472)
-(setq calendar-longitude +116.3887056)
 
 ;; init ccls include path
 (after! ccls
@@ -47,26 +37,6 @@
 
 ;; Set default directory
 (add-hook! emacs-startup (setq default-directory "~"))
-
-;;
-;; Optimize garbage-collect
-;; Related variable: `gc-cons-threshold'
-;; (setq gc-cons-threshold 2147483648)
-;;
-;; Execute `garbage-collect' when emacs is idle for a specified time,
-;; and count the time consumed.
-;; (defmacro k-time (&rest body)
-;;   "Measure and return the time it takes evaluating BODY."
-;;   `(let ((time (current-time)))
-;;      ,@body
-;;      (float-time (time-since time))))
-;; (run-with-idle-timer 300 t
-;;                      (lambda ()
-;;                        (message "Garbage Collector has run for %.06fsec"
-;;                                 (k-time (garbage-collect)))))
-;;
-;; Execute `garbage-collect' when emacs is idle for a specified time.
-;; (run-with-idle-timer 300 t #'garbage-collect)
 
 (setenv "SSH_AUTH_SOCK" "/Users/yaohui/.gnupg/S.gpg-agent.ssh")
 
