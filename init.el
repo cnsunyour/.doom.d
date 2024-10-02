198bc6dc8e8c21078770d85530195256d1ddc1e0
Sunn Yao <sunyour@gmail.com>
2024-10-02 15:34:14 +0800

    enable docker module

--- init.el
+++ init.el
@@ -81,7 +81,7 @@
        ;; ansible
        ;; (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;; (docker +lsp)
+       (docker +lsp)
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
