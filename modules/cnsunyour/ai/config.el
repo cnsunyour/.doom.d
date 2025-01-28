1e1704fcc230417b5e7881cded076c79839b42f6
Sunn Yao <sunyour@gmail.com>
2025-01-29 04:39:15 +0800

    magit-gptcommit: enable magit-gptcommit-mode

--- modules/cnsunyour/ai/config.el
+++ modules/cnsunyour/ai/config.el
@@ -106,7 +106,7 @@
   ;; Enable magit-gptcommit-mode to watch staged changes and generate commit message automatically in magit status buffer
   ;; This mode is optional, you can also use `magit-gptcommit-generate' to generate commit message manually
   ;; `magit-gptcommit-generate' should only execute on magit status buffer currently
-  ;; (magit-gptcommit-mode 1)
+  (magit-gptcommit-mode 1)
 
   ;; Add gptcommit transient commands to `magit-commit'
   ;; Eval (transient-remove-suffix 'magit-commit '(1 -1)) to remove gptcommit transient commands
