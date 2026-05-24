c3417ab8afead0b38e6d5bb21f96f7cfd30b23c6
Sunn Yao <sunyour@gmail.com>
2026-05-24 18:31:58 +0800

    feat(ui): Enable doom-dashboard splash screen

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       ;; doom-dashboard    ; a nifty splash screen for Emacs
+       doom-dashboard    ; a nifty splash screen for Emacs
        ;; doom-quit         ; DOOM quit-message prompts when you quit Emacs
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        ;; indent-guides     ; highlighted indent columns
