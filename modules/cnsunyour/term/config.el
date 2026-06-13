36e4f88625ffbd55b80038da6a106a5780812c37
Sunn Yao <sunyour@gmail.com>
2026-06-14 02:38:31 +0800

    feat(term): add popup rule for ghostel
    Configure ghostel buffer to display as a popup at 25% window height
    with auto-selection and no auto-quit.

--- modules/cnsunyour/term/config.el
+++ modules/cnsunyour/term/config.el
@@ -9,4 +9,5 @@
   :custom
   (ghostel-module-auto-install 'download)
   :config
+  (set-popup-rule! "^\\*ghostel" :size 0.25 :vslot -4 :select t :quit nil :ttl 0)
   (set-evil-initial-state! 'ghostel-mode 'emacs))
